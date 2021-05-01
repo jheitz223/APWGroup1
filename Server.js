d2060d98d43116967bea3be9a34dc467e5bd77a4
jheitz223 <jheitz223@gmail.com>
2021-04-30 22:41:34 -0400

    Added comments

--- Server.js
+++ Server.js
@@ -6,29 +6,36 @@ var Homepage;
 var Quiz;
 //var Leaderboard;
 
+//Read contents of Homepage.html
 fs.readFile("Homepage.html")
+//  Store contents in Homepage variable and start webpage
     .then(contents => {
         Homepage = contents;
         server.listen(port, host, () => {
             console.log(`Server is running on http://${host}:${port}`);
         });
     })
+//  If there's an issue reading the file, throw an error
     .catch(err => {
         console.error(`Could not read Homepage.html file: ${err}`);
         process.exit(1);
     });
 
+//Read contents of Quiz.html
 fs.readFile("Quiz.html")
+//  Store contents in Quiz variable and start webpage
     .then(contents => {
         Quiz = contents;
         server.listen(port, host, () => {
             console.log(`Server is running on http://${host}:${port}`);
         });
     })
+//  If there's an issue reading the file, throw an error
     .catch(err => {
         console.error(`Could not read Quiz.html file: ${err}`);
         process.exit(1);
     });
+
 /*
 fs.readFile("Leaderboard.html")
     .then(contents => {
@@ -42,9 +49,12 @@ fs.readFile("Leaderboard.html")
         process.exit(1);
     });
 */
+
+//Listen for HTTP requests
 const requestListener = function (req, res) {
 
     res.setHeader("Content-Type", "text/html");
+//  Check URL to determine proper webpage to send to client
     switch (req.url) {
         case "/home":
             res.writeHead(200);
@@ -62,4 +72,5 @@ const requestListener = function (req, res) {
     
 }
 
+//Create HTTP server
 const server = http.createServer(requestListener);
